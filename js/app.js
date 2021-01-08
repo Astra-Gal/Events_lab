130b25dd980d7f16c18651238e2aeadbe4fdb759
Astra-Gal <giulia.galastro@gmail.com>
2021-01-08 14:51:50 +0000

    added handleTitleInput function

--- js/app.js
+++ js/app.js
@@ -5,5 +5,42 @@ document.addEventListener('DOMContentLoaded', () => {
   titleInput.addEventListener("input", handleInput);
 });
 
+const handleTitleInput = (event) => {
+  const resultTitleInput = document.querySelector('#title');
+  const resultAuthorInput = document.querySelector('#author');
+  
+}
 
 
+// / const handleInput = (event) => {
+    //   //get value from input
+    //   //update para text
+    //   const resultParagraph = document.querySelector('#input-result');
+    //   resultParagraph.textContent = `You typed: ${event.target.value}`;
+    // }
+
+const newReadingListItem = document.createElement('li');
+
+newReadingListItem.textContent = 
+
+
+// Create a list item
+// Append list item to ul
+
+
+
+
+
+/
+
+// -------
+// const handleFormSubmit = function (event) {
+//   event.preventDefault();
+//   console.log(this);
+  
+//   const resultParagraph = document.querySelector('#form-result');
+//   resultParagraph.textContent = `Your name: 
+//     ${this.first_name.value}
+//     ${this.last_name.value}`;
+
+// }
